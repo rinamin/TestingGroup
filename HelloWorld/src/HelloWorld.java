a0bdc39009bced18873cc663a3b00ac8f131aad9
Alex.T <alexodm@gmail.com>
2019-07-24 19:44:51 +0600

    master commit

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("This is the testing OUT");
+        System.out.println("This is master OUT");
     }
 }
