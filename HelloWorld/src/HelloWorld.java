4c0ef6c8fec1a595b9c7b9730ce5adc1bf2b7f12
Alex.T <alexodm@gmail.com>
2019-07-24 20:00:44 +0600

    Cherry Pick commit

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("This is test branch OUT");
+        System.out.println("This is test cherry pick branch OUT");
     }
 }
