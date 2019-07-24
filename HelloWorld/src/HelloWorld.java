849bedaca72282bfa94d1a8cbd09920a1104f6cf
Alex.T <alexodm@gmail.com>
2019-07-24 19:40:19 +0600

    Hello commit

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World, this is my first out!");
+        System.out.println("This is the testing OUT");
     }
 }
