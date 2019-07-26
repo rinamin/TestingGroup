813b664b6e78ccdcb0d721e4bc6906fa5574150c
RinaMineeva <Rmineeva14@gmail.com>
2019-07-26 13:02:52 +0600

    change

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("This is master OUT");
-        System.out.println("This is test cherry pick branch OUT");
+        System.out.println("This is test blue branch");
     }
 }
