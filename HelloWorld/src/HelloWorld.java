ce2da6ffe2730c5877a64bf39448dacc19791789
RinaMineeva <Rmineeva14@gmail.com>
2019-07-26 14:10:27 +0600

    12345

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("This is master OUT");
         System.out.println("This is test cherry pick blue branch");
+        System.out.println("12345");
     }
 }
